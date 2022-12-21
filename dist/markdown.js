e5bd5d47a03d59f469183eca4a781d0b6e754cd1
lacnoskillz <kaikane3457@hotmail.com>
2022-12-20 23:03:08 -0600

    got markdown.js to make cards for all members entered in promps. added hrefs for githubs and sendto: for emails

--- dist/markdown.js
+++ dist/markdown.js
@@ -1,64 +1,108 @@
-function generate(team){
+function generate(Manager1, engineerarray, internarray) {
+  console.log("this the manager", Manager1);
+  console.log("engineers", engineerarray);
+  console.log("interns", internarray);
 
 
-function engineersection(engineer){
-      if(engineer == null){
-            return ""
-    }else{
-            return x = `<div class="card" style="width: 18rem;">
+  function internsection(data) {
+    if (data == null) {
+      return ""
+    } else {
+
+      return `<div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">${data.name}</h5>
+            <p class="card-text">Occupation: Intern</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item" id="id">ID: ${data.id}</li>
+            <li class="list-group-item" id="email">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+            <li class="list-group-item" id="office">School: ${data.school}</li>
+          </ul>
+        </div>`
+    }
+  }
+
+  function engineersection(data) {
+    if (data == null) {
+      return ""
+    } else {
+      return `<div class="card" style="width: 18rem;">
             <div class="card-body">
-              <h5 class="card-title">${data.engineer}</h5>
-              <p class="card-text">Ocupation</p>
+              <h5 class="card-title">${data.name}</h5>
+              <p class="card-text">Occupation: Engineer</p>
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item" id="id">ID: ${data.id}</li>
-              <li class="list-group-item" id="email">Email: ${data.email}</li>
-              <li class="list-group-item" id="office">Office number: ${data.github}</li>
+              <li class="list-group-item" id="email">Email:<a href="mailto:${data.email}">${data.email}</a></li>
+              <li class="list-group-item" id="office">Github: <a href="https://www.github.com/${data.github}" target="_blank">${data.github}</a></li>
             </ul>
           </div>`
     }
-}
-for(var i=0; i<team.length; i++){
-
-
-}
-function generateMarkdown(data) {
- 
-      const engineersection = engineersection();
-        return `
-        <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>My Team</h1>
-    <div class="container">
-        <div class="card" style="width: 18rem;">
+  }
+  function managersection(data) {
+    return `<div class="card" style="width: 18rem;">
             <div class="card-body">
-              <h5 class="card-title">${data.manager}</h5>
-              <p class="card-text">Ocupation</p>
+              <h5 class="card-title">${data.name}</h5>
+              <p class="card-text">Occupation: Manager</p>
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item" id="id">ID: ${data.id}</li>
-              <li class="list-group-item" id="email">Email: ${data.email}</li>
+              <li class="list-group-item" id="email">Email: <a href="mailto:${data.email}">${data.email}</a></li>
               <li class="list-group-item" id="office">Office number: ${data.office}</li>
             </ul>
-          </div>
-      ${engineersection}   
+          </div>`
+  }
 
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</body>
+  function generateMarkdown(Manager1, engineerarray, internarray) {
 
-</html>`;
+    // Potential way to go about it, have one single members array and loop through
+    // for (let i = 0; i < members.length; i++) {
+    //   let member = members[i];
+    //   if (member.getRole() === 'Engineer') {
+    //     engineersection(member)
+    //   }
+    // }
+    var x = '';
+    var y = '';
+    var z = managersection(Manager1);
+    for (var i = 0; i < engineerarray.length; i++) {
+      x += engineersection(engineerarray[i]);
+    }
+    for (var b = 0; b < internarray.length; b++) {
+      y += internsection(internarray[b]);
+    }
+  
+    return `
+           <!DOCTYPE html>
+   <html lang="en">
+   
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="stylesheet" href="./dist/style.css">
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+       <title>Document</title>
+   </head>
+   
+   <body>
+       <h1>My Team</h1>
+       <div class="container">
+       ${z}
+       ${x}
+       ${y}
+   
+       </div>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+   </body>
+   
+   </html>`;
+  }
+
+  const ThelastMarkdown = generateMarkdown(Manager1, engineerarray, internarray)
+  return ThelastMarkdown;
 }
-}
+
+
 module.exports = generate;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const markdown = require('./dist/markdown.js');
-const Employee = require("./Employee");
-
+//const markdown2 = require('./dist/markdown2')
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer")
 const managerquestions = [
     {
         type: 'input',
@@ -12,7 +14,8 @@ const managerquestions = [
     {
         type: 'input',
         message: 'Enter manager employee ID',
-        name: 'id'
+        name: 'id',
+        
     },
     {
         type: 'input',
@@ -20,7 +23,7 @@ const managerquestions = [
         name: 'email'
     },
     {
-        type: 'input',
+        type: 'number',
         message: 'Enter Office number',
         name: 'office'
     },
@@ -85,49 +88,54 @@ const internquestions = [
         message: 'Add members?',
         name: 'add',
         choices: ['engineer', 'intern', 'finish']
-        
+
     }
 ];
 
-
-engineerarray =[];
+let Manager1 = 0;
+engineerarray = [];
 internarray = [];
 questionsasked = managerquestions;
-function init(questionsasked){
-    if(questionsasked == engineerquestions){
+function init(questionsasked) {
+
+    inquirer
+        .prompt(questionsasked)
+
+        .then((data) => {
 
 
-    }if(questionsasked == internquestions){
+            if (questionsasked == managerquestions) {
+                Manager1 = new Manager(data.manager, data.id, data.email, data.office);
+                console.log("manager1", Manager1)
+            } if (questionsasked == engineerquestions) {
+                let Engineer1 = new Engineer(data.engineer, data.id, data.email, data.github);
+                engineerarray.push(Engineer1);
+                console.log("engineer array:", engineerarray);
+            } if (questionsasked == internquestions) {
+                let Intern1 = new Intern(data.intern, data.id, data.email, data.school);
+                internarray.push(Intern1);
+                console.log("intern array", internarray);
+            }
 
-    }
-inquirer
-    .prompt(questionsasked)
-   
-    .then((data) => {
-        const Manager1 = new Managers(data.manager,data.id,data.email,data.office);
-        
-    
-        if(data.add == 'engineer'){
-            const Engineer1 = new Engineer(data.engineer,data.id,data.email,data.github);
-            engineerarray.push(Engineer1);
-            questionsasked= engineerquestions;
-            init(questionsasked);
-        }
-      if(data.add == 'intern'){
-        const Intern1 = new Intern(data.manager,data.id,data.email,data.school);
-        internarray.push(Intern1);
-           questionsasked = internquestions;
-            init(questionsasked);
-        }
-    if(data.add == 'finish'){
-        const filename = 'sampleindex.html';
-     const genereatedmarkdown =  markdown([Manager1,...engineerarray,...internarray]);
-     fs.writeFile(filename, markdown,
+            if (data.add == 'engineer') {
+                questionsasked = engineerquestions;
+                init(questionsasked);
+            }
+            if (data.add == 'intern') {
 
-        (err) => err ? console.log(err) : console.log('page generating...')
-      );
-    }
-     
-  });
+                questionsasked = internquestions;
+                init(questionsasked);
+            }
+            if (data.add == 'finish') {
+                const filename = 'sampleindex.html';
+                //const generatemarkdown2 = markdown2(data);
+                const genereatedmarkdown = markdown(Manager1, engineerarray, internarray);
+                fs.writeFile(filename, genereatedmarkdown,
+
+                    (err) => err ? console.log(err) : console.log('page generating...')
+                );
+            }
+
+        });
 }
-  init(questionsasked);
+init(questionsasked);
